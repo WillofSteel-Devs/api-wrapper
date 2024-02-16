bb0a1ec232ec5aa3b520c62fbcd32dc7c6578713
ItsNeil <68216747+ItsNeil17@users.noreply.github.com>
2024-02-16 22:29:47 +0530

    Add get_all_offers

--- willofsteel/wrapper.py
+++ willofsteel/wrapper.py
@@ -14,17 +14,19 @@ import requests
 import asyncio
 import atexit
 
-from .types import Player, Alliance
-
-BASE = "https://api.wos.itsneil.tech"
+from .types import Player, Alliance, MarketOrder
+from .constants import BASE, ALL_ITEMS
+from .utils import parse_error
+from .exceptions import *
 
 class Wrapper:
     def __init__(self, api_key: str):
         self.api_key = api_key
         self.headers = {
             "API-Key": self.api_key,
+            "User-Agent": "Will of Steel API Wrapper",
             "Content-Type": "application/json"
-        }
+        }      
         # self._verify_key() # not implemented in API as of now
 
     def _verify_key(self) -> None:
@@ -41,8 +43,7 @@ class Wrapper:
         response = self.request("GET", "/alliance", headers=self.headers)
         status = response.status
         if status == 400:
-            # raise NotInAlliance() # custom errors still left to do
-            raise KeyError
+            raise NotInAlliance
         data = response.json()
         return Alliance.from_response(data)
 
@@ -57,8 +58,11 @@ class Wrapper:
         if status == 200:
             return True
         else:
-            return False # TODO: add better error management
-            
+            json = response.json()
+            parse_error(json["detail"])
+            print(json["detail"])
+            print("This error was not automatically detected, please report this to the maintainers (or fix it yourself)!")
+
     def update_alliance_user_limit(self, new_limit: str) -> bool:
         if new_limit > 50:
             return KeyError # return an error which says it cant be more than 50 
@@ -70,19 +74,40 @@ class Wrapper:
         if status == 200:
             return True
         else:
-            return False # TODO: add better error management
+            json = response.json()
+            parse_error(json["detail"])
+            print(json["detail"])
+            print("This error was not automatically detected, please report this to the maintainers (or fix it yourself)!")
             
-    def request(self, method: Literal["GET", "POST"], route: str, headers: dict):
-        headers = {
-            "User-Agent": "Will of Steel API Wrapper",
-            "API-Key": self.api_key,
-            "Content-Type": "application/json"
+    def get_all_offers(self, offer_type: Literal["buy", "sell"]):
+        if offer_type not in ["buy", "sell"]:
+            raise KeyError("Invalid offer type")
+        offers = []
+        params = {
+            "order_type": offer_type,
         }
+        for item_id in ALL_ITEMS:
+            print(item_id)
+            params["item_type"] = item_id
+            response = self.request("GET", "/market", headers=self.headers, params=params)
+            json_data = response.json()
+            if response != 200:
+                parse_error(json_data["detail"])
+            number_of_orders = len(json_data["orders"])
+            if number_of_orders == 0:
+                continue
+            for order_uuid, order_data in json_data["orders"].items():
+                offers.append(MarketOrder.from_response(order_uuid, order_data))
+        return offers
+
+    def request(self, method: Literal["GET", "POST"], route: str, headers: dict, params: dict = None):
         url = BASE + route
 
         if method not in ["GET", "POST"]:
             return KeyError("Invalid Method")
 
-        response = requests.request(method, url, headers=headers)
+        response = requests.request(method, url, headers=headers, params=params)
         response.status = response.status_code
+        if response.status == 500:
+            raise ServerError
         return response
