2842f292150ae887e06c80362b8304671a78f6dd
ItsNeil17 <git@itsneil.tech>
2024-06-10 14:16:09 +0530

    feat: Add convert_str_to_UT function for string to UnitType conversion

--- willofsteel/types/__init__.py
+++ willofsteel/types/__init__.py
@@ -1,7 +1,7 @@
 # ruff: noqa
 from .alliance import Alliance
 from .player import Player
-from .troops import UnitType, UnitProperties
+from .troops import UnitType, UnitProperties, convert_str_to_UT
 from .market import MarketOrder
 from .logs import LoggingObject
 from .items import ItemType, convert_str_to_IT
--- willofsteel/types/troops.py
+++ willofsteel/types/troops.py
@@ -16,3 +16,17 @@ class UnitType(UnitProperties, Enum):
     BIG_BOWMEN = UnitProperties("Big Bowmen", 15, 55)
     HEAVY_MEN = UnitProperties("Heavy Men", 15, 85)
     KINGS_GUARDS = UnitProperties("King's Guards", 30, 155)
+
+StringToUnitTypeConversion = {
+    "infantry": UnitType.INFANTRY,
+    "cavalry": UnitType.CAVALRY,
+    "artillery": UnitType.ARTILLERY,
+    "assassins": UnitType.ASSASSINS,
+    "bowmen": UnitType.BOWMEN,
+    "big_bowmen": UnitType.BIG_BOWMEN,
+    "heavy_men": UnitType.HEAVY_MEN,
+    "kings_guards": UnitType.KINGS_GUARDS
+}
+
+def convert_str_to_UT(unit_str: str) -> UnitType:
+    return StringToUnitTypeConversion[unit_str]
